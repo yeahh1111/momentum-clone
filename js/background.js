487ad3c01d285c95f4787c7a22899698ea9f47dd
yeahh1111 <84653062+yeahh1111@users.noreply.github.com>
2021-09-04 23:56:20 +0900

    imgsrc

--- js/background.js
+++ js/background.js
@@ -1,7 +1,11 @@
-const images = ["01.png", "02.png", "03.png"];
+const images = [
+  "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbgV7ma%2FbtqACPJ6BWu%2FhEYxW2SwvlykokkUXp3ns0%2Fimg.jpg",
+  "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbUNFlB%2FbtqACOEoR8f%2FaXKpiyD7FVRtIN4Ok31PPk%2Fimg.jpg",
+  "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbxHjgs%2FbtqABrczePG%2FsEVQR6LAIC62zm2okDD761%2Fimg.jpg",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.querySelector("#background-img");
 
-bgImage.src = `/img/${chosenImage}`;
+bgImage.src = `${chosenImage}`;
